55d3e69cdec1d8b34cc317e01f0d7483e7ac5a0f
Lê Nguyễn Khánh Duy <82671640+lnkhanhduy@users.noreply.github.com>
2023-05-28 15:21:24 +0700

    .

--- db/ConnectDB.js
+++ db/ConnectDB.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@laptop-store.umcasgn.mongodb.net/laptop-store?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@laptop-store.umcasgn.mongodb.net/laptop-store?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
     );
     console.log('MongoDB connected');
   } catch (error) {
